26edc4f079dfd0e56d4e7c3372ef26948870737e
xiangcm1 <xiangcm1163@163.com>
2021-01-23 10:31:57 +0800

    SpringBoot helloWord

---
+++ src/main/java/com/xiangcm/HelloWordApplication.java
@@ -0,0 +1,18 @@
+package com.xiangcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: HelloWordApplication
+ * @Description: 主启动类
+ * @Author: DELL
+ * @Date: 2021/1/22 23:19
+ **/
+@SpringBootApplication
+public class HelloWordApplication {
+    // 启动Spring应用
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWordApplication.class,args);
+    }
+}
---
+++ src/main/java/com/xiangcm/controller/HelloWordController.java
@@ -0,0 +1,21 @@
+package com.xiangcm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * @ClassName: HelloWordController
+ * @Description: controller
+ * @Author: DELL
+ * @Date: 2021/1/22 23:26
+ **/
+@Controller
+public class HelloWordController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello word!";
+    }
+}
